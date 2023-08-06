9367edd839a38c4f50c08e9b8f69d4eae8f36e89
John Chan <johnchancontact@gmail.com>
2023-08-06 09:05:20 -0400

    removed unnecessary @staticmethod from Users class

--- server.py
+++ server.py
@@ -2,28 +2,35 @@ import os
 import json
 from flask import Flask, request, jsonify
 from bson import json_util, ObjectId
-import pymongo
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# Environment
 database_url = os.getenv("DATABASE_URL")
 cluster_name = os.getenv("MONGODB_CLUSTER")
 
+# db connection
 cluster = MongoClient(database_url)
 db = getattr(cluster, cluster_name)
 collection = db["users"]
 
 app = Flask(__name__)
 
+# No in use yet
+
 
 def parse_json(data):
     return json.loads(json_util.dumps(data))
 
+# # Convert BSON data to Python dictionary using json_util
+# data_python = json_util.loads(json_util.dumps(data_bson))
+
+# # Serialize Python dictionary to JSON response using jsonify()
+# response = jsonify(data_python)
+
 
 class Users:
-    @staticmethod
     @app.route('/users', methods=['GET'])
     # Add the fields you want returned into the pipeline -ex: "email": 1
     def get_users():
@@ -33,14 +40,12 @@ class Users:
         users = list(collection.aggregate(pipeline))
         return jsonify({"users": users})
 
-    @staticmethod
     @app.route('/users', methods=['POST'])
     def create_user():
         data = request.get_json()
         collection.insert_one(data)
         return jsonify({"message": "User created successfully"})
 
-    @staticmethod
     @app.route('/users/<user_id>', methods=['GET'])
     def get_user(user_id):
         user = collection.find_one({"_id": ObjectId(user_id)})
@@ -50,14 +55,12 @@ class Users:
         else:
             return jsonify({"message": "User not found"})
 
-    @staticmethod
     @app.route('/users/<user_id>', methods=['PUT'])
     def update_user(user_id):
         data = request.get_json()
         collection.update_one({"_id": ObjectId(user_id)}, {"$set": data})
         return jsonify({"message": "User updated successfully"})
 
-    @staticmethod
     @app.route('/users/<user_id>', methods=['DELETE'])
     def delete_user(user_id):
         result = collection.delete_one({"_id": ObjectId(user_id)})
